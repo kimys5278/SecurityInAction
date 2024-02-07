ce804312e364622f61577d573f2b36d993b5c3d6
Kim-Yu-Seong <107562291+kimys5278@users.noreply.github.com>
2024-02-07 17:11:06 +0900

    Add files via upload
    권한 부여 구성 - 액세스 제한, 제한 적용

---
+++ ssia-ch8-ex/src/main/java/com/springboot/ssiach8ex/SsiaCh8ExApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.ssiach8ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsiaCh8ExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsiaCh8ExApplication.class, args);
+    }
+
+}
---
+++ ssia-ch8-ex/src/test/java/com/springboot/ssiach8ex/SsiaCh8ExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.ssiach8ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsiaCh8ExApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
